d87a9f8376f54e82e6561df528acc4158b34d796
dbvulpe <77484353+dbvulpe@users.noreply.github.com>
2021-01-15 19:33:27 +0800

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -2,9 +2,9 @@ const mysql = require("mysql");
 
 parameters = {
   host: "localhost",
-  port: 3306,
+  port: 3300,
   user: "root",
-  password: "password", // type your own password here
+  password: "enter your password", // type your own password here
   database: "nusmoney", // replace with the name of the databse you used on day 1
 };
 
